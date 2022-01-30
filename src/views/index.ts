d714fd5db299e17f23080e3693e8029e64bd6491
Luciana Moll <lucianam910@gmail.com>
2022-01-30 10:29:55 -0300

    feat: add instructions page

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,4 +1,4 @@
-export { Home } from "./Home";
+export { Home, Instructions } from "./Home";
 export { Game } from "./Game";
 export { Credits } from "./Credits";
 export { Confirmation } from "./Confirmation";
