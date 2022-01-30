eec017674844540f01a977416589c6b1ffb96884
twjasa <twjasa@gmail.com>
2022-01-29 23:03:11 -0300

    fix: error console

--- src/gameObjects/WallBlock/WallBlock.tsx
+++ src/gameObjects/WallBlock/WallBlock.tsx
@@ -1,52 +1,72 @@
-import React from 'react';
-import { useConstructGameObject } from '../useConstructGameObject'
-import type { GameObject } from '../../sharedTypes'
+import React from "react";
+import { useConstructGameObject } from "../useConstructGameObject";
+import type { GameObject } from "../../sharedTypes";
 import { CollisionCategories } from "../../store/Physics";
-import { CELL_HEIGHT, CELL_WIDTH } from '../../settings';
+import { CELL_HEIGHT, CELL_WIDTH } from "../../settings";
 
-
-const LeftBump: React.ComponentType<React.SVGProps<SVGSVGElement>> = ({ x, y }) => (
-  <svg x={x} y={y}  width={CELL_WIDTH} height={CELL_HEIGHT}>
-    <line stroke-width="50" y2="100%" x2="0" y1="0" x1="0" stroke="#000" />
-    <rect x={0} y={0} width={CELL_WIDTH} height={CELL_HEIGHT} fill='none' />
+const LeftBump: React.ComponentType<React.SVGProps<SVGSVGElement>> = ({
+  x,
+  y,
+}) => (
+  <svg x={x} y={y} width={CELL_WIDTH} height={CELL_HEIGHT}>
+    <line strokeWidth="50" y2="100%" x2="0" y1="0" x1="0" stroke="#000" />
+    <rect x={0} y={0} width={CELL_WIDTH} height={CELL_HEIGHT} fill="none" />
   </svg>
-)
-const TopBump: React.ComponentType<React.SVGProps<SVGSVGElement>> = ({ x, y }) => (
-  <svg x={x} y={y}  width={CELL_WIDTH} height={CELL_HEIGHT}>
-    <line stroke-width="50" y2="0" x2="100%" y1="0" x1="0" stroke="#000" />
-    <rect x={0} y={0} width={CELL_WIDTH} height={CELL_HEIGHT} fill='none' />
+);
+const TopBump: React.ComponentType<React.SVGProps<SVGSVGElement>> = ({
+  x,
+  y,
+}) => (
+  <svg x={x} y={y} width={CELL_WIDTH} height={CELL_HEIGHT}>
+    <line strokeWidth="50" y2="0" x2="100%" y1="0" x1="0" stroke="#000" />
+    <rect x={0} y={0} width={CELL_WIDTH} height={CELL_HEIGHT} fill="none" />
   </svg>
-)
+);
 
-const RightBump: React.ComponentType<React.SVGProps<SVGSVGElement>> = ({ x, y }) => (
-  <svg x={x} y={y}  width={CELL_WIDTH} height={CELL_HEIGHT}>
-    <line stroke-width="50" y2="100%" x2="100%" y1="0" x1="100%" stroke="#000" />
-    <rect x={0} y={0} width={CELL_WIDTH} height={CELL_HEIGHT} fill='none' />
+const RightBump: React.ComponentType<React.SVGProps<SVGSVGElement>> = ({
+  x,
+  y,
+}) => (
+  <svg x={x} y={y} width={CELL_WIDTH} height={CELL_HEIGHT}>
+    <line strokeWidth="50" y2="100%" x2="100%" y1="0" x1="100%" stroke="#000" />
+    <rect x={0} y={0} width={CELL_WIDTH} height={CELL_HEIGHT} fill="none" />
   </svg>
-)
-const BottomBump: React.ComponentType<React.SVGProps<SVGSVGElement>> = ({ x, y }) => (
-  <svg x={x} y={y}  width={CELL_WIDTH} height={CELL_HEIGHT}>
-    <line stroke-width="50" y2="100%" x2="100%" y1="100%" x1="0" stroke="#000" />
-    <rect x={0} y={0} width={CELL_WIDTH} height={CELL_HEIGHT} fill='none' />
+);
+const BottomBump: React.ComponentType<React.SVGProps<SVGSVGElement>> = ({
+  x,
+  y,
+}) => (
+  <svg x={x} y={y} width={CELL_WIDTH} height={CELL_HEIGHT}>
+    <line strokeWidth="50" y2="100%" x2="100%" y1="100%" x1="0" stroke="#000" />
+    <rect x={0} y={0} width={CELL_WIDTH} height={CELL_HEIGHT} fill="none" />
   </svg>
-)
-
-
+);
 
 const WALL_TYPE = {
   LEFT_BUMP: LeftBump,
   TOP_BUMP: TopBump,
   RIGHT_BUMP: RightBump,
   DOWN_BUMP: BottomBump,
-} as const
+} as const;
 
 interface WallBlockProps extends GameObject {
-  type: keyof typeof WALL_TYPE
+  type: keyof typeof WALL_TYPE;
 }
 
 export function WallBlock(props: WallBlockProps) {
-  const { size } = useConstructGameObject({ ...props, collisionFilterCategory: CollisionCategories.WALL });
-  const { type = 'TOP_BUMP', ...rest } = props;
+  const { size } = useConstructGameObject({
+    ...props,
+    collisionFilterCategory: CollisionCategories.WALL,
+  });
+  const { type = "TOP_BUMP", ...rest } = props;
   const WallBlockSvg = WALL_TYPE[type];
-  return <WallBlockSvg  {...rest}  x={size.min.x} y={size.min.y} width={CELL_WIDTH} height={CELL_HEIGHT} />;
+  return (
+    <WallBlockSvg
+      {...rest}
+      x={size.min.x}
+      y={size.min.y}
+      width={CELL_WIDTH}
+      height={CELL_HEIGHT}
+    />
+  );
 }
